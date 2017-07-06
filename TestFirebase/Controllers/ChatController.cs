891f1a0677719695d3221dabdf9fbbc74d639440
dangnh2002 <dangnh2002@gmail.com>
2017-07-06 17:01:56 +0700

    commit code

---
+++ TestFirebase/Controllers/ChatController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TestFirebase.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        [HttpPost]
+        public ActionResult Index(int channel)
+        {
+            if (Session["ID_Channel"].ToString() == channel.ToString())
+            {
+                ViewBag.ID_channel = channel;
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public ActionResult OutChannel()
+        {
+            Session["ID_Channel"] = "";
+            return Redirect("/");
+        }
+        public ActionResult LogOut()
+        {
+            //đăng xuất tài khoản
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            FormsAuthentication.RedirectToLoginPage();
+            return Redirect("/");
+        }
+    }
+}
